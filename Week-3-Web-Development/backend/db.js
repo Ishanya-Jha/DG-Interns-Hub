d35114d59824a8dc12b3145f0ee7d7ae288ddb0f
Ishanya-Jha <ishanyajha172@gmail.com>
2026-09-07 11:40:05 +0530

    Update db.js

--- Week-3-Web-Development/backend/db.js
+++ Week-3-Web-Development/backend/db.js
@@ -1,1 +1,19 @@
-ok
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ecommerce_db"
+});
+
+db.connect((error) => {
+    if (error) {
+        console.error("MySQL connection failed:", error.message);
+        return;
+    }
+
+    console.log("MySQL connected successfully!");
+});
+
+module.exports = db;
